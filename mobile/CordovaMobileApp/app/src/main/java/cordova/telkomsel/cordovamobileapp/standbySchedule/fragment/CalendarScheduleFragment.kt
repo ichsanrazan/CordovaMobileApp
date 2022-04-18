657943e8ac58ef02b5c15b547ef085ad9e43fbe6
Kaleb Juliu <kalebjuliu@gmail.com>
2022-04-18 11:59:17 +0700

    Added bottom navigation bar on Standby Schedule

---
+++ mobile/CordovaMobileApp/app/src/main/java/cordova/telkomsel/cordovamobileapp/standbySchedule/fragment/CalendarScheduleFragment.kt
@@ -0,0 +1,12 @@
+package cordova.telkomsel.cordovamobileapp.standbySchedule.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import cordova.telkomsel.cordovamobileapp.R
+
+class CalendarScheduleFragment: Fragment(R.layout.fragment_calendar_schedule) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ mobile/CordovaMobileApp/app/src/main/java/cordova/telkomsel/cordovamobileapp/standbySchedule/fragment/MessageScheduleFragment.kt
@@ -0,0 +1,12 @@
+package cordova.telkomsel.cordovamobileapp.standbySchedule.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import cordova.telkomsel.cordovamobileapp.R
+
+class MessageScheduleFragment: Fragment(R.layout.fragment_message_schedule) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
