55e600b998efcb49813889e8529d297f5914ab78
Kaleb Juliu <kalebjuliu@gmail.com>
2022-05-19 14:41:00 +0700

    Merge ichsan

--- mobile/CordovaMobileApp/app/src/main/java/cordova/telkomsel/cordovamobileapp/retrofit/RetrofitInstance.kt
+++ mobile/CordovaMobileApp/app/src/main/java/cordova/telkomsel/cordovamobileapp/retrofit/RetrofitInstance.kt
@@ -14,13 +14,13 @@ class RetrofitInstance {
         val baseUrl = "https://restapi-activitylog.000webhostapp.com/restapi_activitylog/api/"
 
         //Login
-        //val baseUrl2 = "http://192.168.1.13/mobileapp/mobile-app/restapi_authentication/"
+        val baseUrl2 = "http://192.168.1.13/mobileapp/mobile-app/restapi_authentication/"
         //val baseUrl2 = "http://192.168.18.179:8080/restapi_authentication/"
-        val baseUrl2 = "https://restapi-activitylog.000webhostapp.com/restapi_authentication/"
+        //val baseUrl2 = "https://restapi-activitylog.000webhostapp.com/restapi_authentication/"
 
 
-        //val baseUrl3 = "http://192.168.18.179:8080/api_standby/"
-        val baseUrl3 = "http://192.168.1.13/mobileapp/mobile-app/api_standby/"
+        val baseUrl3 = "http://192.168.18.179:8080/api_standby/"
+        //val baseUrl3 = "http://192.168.1.13/mobileapp/mobile-app/api_standby/"
 
         fun getRetroInstance(): Retrofit {
             val logging = HttpLoggingInterceptor()
