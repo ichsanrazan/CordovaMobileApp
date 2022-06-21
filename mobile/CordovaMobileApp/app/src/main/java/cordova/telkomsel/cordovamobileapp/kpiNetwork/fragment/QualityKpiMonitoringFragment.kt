1213437c2ae8a79c73cea0995869a7d25da9a889
Kaleb Juliu <kalebjuliu@gmail.com>
2022-06-21 10:24:09 +0700

    Added new fragment for Quality Monitoring

--- mobile/CordovaMobileApp/app/src/main/java/cordova/telkomsel/cordovamobileapp/kpiNetwork/fragment/QualityKpiMonitoringFragment.kt
+++ mobile/CordovaMobileApp/app/src/main/java/cordova/telkomsel/cordovamobileapp/kpiNetwork/fragment/QualityKpiMonitoringFragment.kt
@@ -1,4 +1,13 @@
 package cordova.telkomsel.cordovamobileapp.kpiNetwork.fragment
 
-class QualityKpiMonitoringFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import cordova.telkomsel.cordovamobileapp.R
+import kotlinx.android.synthetic.main.fragment_kpi_quality_monitoring.*
+
+class QualityKpiMonitoringFragment: Fragment(R.layout.fragment_kpi_quality_monitoring) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
